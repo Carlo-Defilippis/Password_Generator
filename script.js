b95c82ff4029855957bc1c336a7c50e1b9ac6d52
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-07-23 17:03:37 -0400

    Basic layout of code

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numbers = "1234567890".split("");
+var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var specialChar = "!#$%&*+<=>?@/[]^_`|~".split("");
+
+
+// User Prompts
+var howManyChar = prompt("How many characters would you like your password to be?");
+var userLetters = confirm("Press OK if you want letters in your password.");
+var userNumbers = confirm("Press OK if you want numbers in your password.");
+var userSpecialChar = confirm("Press OK if you want special characters in your password.");
+
+
+
+function generatePassword(howMany, let, numb, char) {
+  if (let === true) {
+    console.log("User set letters to True");
+  } else {
+    console.log("User set letters to False");
+  }
+  if (numb === true) {
+    console.log("User set numbers to True");
+  } else {
+    console.log("User set numbers to False");
+  }
+  if (char === true) {
+    console.log("User set characters to True");
+  } else {
+    console.log("User set characters to False");
+  }
+  console.log("User requested a password with a length of " + howMany);
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(howManyChar,userLetters,userNumbers,userSpecialChar);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
