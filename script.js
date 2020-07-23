5dbb8024770e4c758ad3b3ff705aced8b7697d04
shotgunmech <47787927+shotgunmech@users.noreply.github.com>
2020-07-23 18:08:40 -0400

    Second Basic layout of code

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 var numbers = "1234567890".split("");
 var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var specialChar = "!#$%&*+<=>?@/[]^_`|~".split("");
-
+var blah = [1,2]
 
 // User Prompts
 var howManyChar = prompt("How many characters would you like your password to be?");
@@ -11,9 +11,11 @@ var userLetters = confirm("Press OK if you want letters in your password.");
 var userNumbers = confirm("Press OK if you want numbers in your password.");
 var userSpecialChar = confirm("Press OK if you want special characters in your password.");
 
-
+howManyChar;
+console.log(letters.join(""))
 
 function generatePassword(howMany, let, numb, char) {
+
   if (let === true) {
     console.log("User set letters to True");
   } else {
@@ -30,13 +32,15 @@ function generatePassword(howMany, let, numb, char) {
     console.log("User set characters to False");
   }
   console.log("User requested a password with a length of " + howMany);
+  return letters.join("")
 }
 
 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(howManyChar,userLetters,userNumbers,userSpecialChar);
+
+  var password = generatePassword(howManyChar, userLetters, userNumbers, userSpecialChar);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
